f7ff17c17a09ac05090ad19395310479d558099f
Ruiling Zhang <ruz152@pitt.edu>
2025-06-12 14:51:52 -0400

    updated to generate migration file for specific repo onto active config

--- modules/aspace_findingaid/src/Form/FindingAidResourceForm.php
+++ modules/aspace_findingaid/src/Form/FindingAidResourceForm.php
@@ -5,8 +5,6 @@ namespace Drupal\aspace_findingaid\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Config\FileStorage;
-use Drupal\Core\Site\Settings;
-use Drupal\Core\Cache\Cache;
 
 /**
  * Configure Archivesspace finding aid migration settings
@@ -32,8 +30,6 @@ class FindingAidResourceForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-   $source_config = \Drupal::config('migrate_plus.migration.aspace_findingaid')->get('source');
-   
     $form['connection'] = [
       '#type' => 'details',
       '#title' => t('ArchivesSpace API Connection Settings'),
@@ -62,7 +58,7 @@ class FindingAidResourceForm extends ConfigFormBase {
       $form['archivesspace_repository'] = [                                     
       '#type' => 'number',                                                 
       '#title' => $this->t('Repository to process migration'),                          
-      '#default_value' => $source_config['repository'],
+      '#default_value' => '',
       '#config_target' => 'aspace_findingaid.settings:archivesspace_repository',
       '#description' => t('The Archivesspace repository to process migration'), 
       '#required' => TRUE,
@@ -75,30 +71,29 @@ class FindingAidResourceForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // generate the migration YML file
-
-    //locate the orginal file
-    $conf_filepath = \Drupal::service('extension.list.module')->getPath('aspace_findingaid').'/config/install';
+    //locate the template file from module samples configuration directory
+    $conf_filepath = \Drupal::service('extension.list.module')->getPath('aspace_findingaid').'/config/samples';
     $file_storage = new FileStorage($conf_filepath);
   
     $file_template = $file_storage->read('migrate_plus.migration.aspace_findingaid');
     if (!$file_template) {
-	\Drupal::logger('aspace_findingaid')->error('Failed to find Archivesspace Finding Aid Migration YAM in configuration install directory');
+	\Drupal::logger('aspace_findingaid')->error('Failed to find Archivesspace Finding Aid Migration Template YAM in configuration samples directory');
         return;
 	}
     //update repository value from configuration form
-    $config = \Drupal::config('migrate_plus.migration.aspace_findingaid');
     $file_template['source']['repository'] = (int)$form_state->getValue('archivesspace_repository');
-    $file_template['label'] = $config->get('label'). $file_template['source']['repository'];
-    $file_template['id'] = $config->get('id').'_'.$file_template['source']['repository'];
-    $config_sync_directory = Settings::get('config_sync_directory');
-   
-    $new_repo_file = "$config_sync_directory/migrate_plus.migration.aspace_findingaid_" . $file_template['source']['repository'] .".yml";
-    $encode_content = \Drupal::service('serialization.yaml')->encode($file_template);
-    file_put_contents($new_repo_file, $encode_content);
+    $file_template['label'] = $file_template['label']. $file_template['source']['repository'];
+    $file_template['id'] = $file_template['id'].'_'.$file_template['source']['repository'];
+  
+    //save to active configuration
+    $config = \Drupal::configFactory()->getEditable('migrate_plus.migration.aspace_findingaid_'.$file_template['source']['repository']);   
+    $config->setData($file_template)->save();
+    \Drupal::logger('aspace_findingaid')->info('A new migration configuration for repository: @repo has been created', 
+			['@repo'=>$file_template['source']['repository'] ]);
 
-    //clearcache and sync
-    drupal_flush_all_caches();
+    //clear migration cache
+    \Drupal::service('plugin.manager.migration')->clearCachedDefinitions();
+
     parent::submitForm($form, $form_state);
   }
 
